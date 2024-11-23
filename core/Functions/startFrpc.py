6cfbe7c4aa9234ecfa4596b04cc071a07b34aa6b
枫相 <140891522+FengXiang2233@users.noreply.github.com>
2024-11-23 18:36:56 +0800

    优化启动frpc

--- core/Functions/startFrpc.py
+++ core/Functions/startFrpc.py
@@ -4,4 +4,6 @@ import core.g_var
 import os
 
 def startFrpc(tun_id:Union[str,int]):
-    os.system(f'start \"frpc - {tun_id}\" cmd /k \"{os.getcwd()}/res/frpc.exe -u {core.g_var.User.token} -p {tun_id}\"')
+    with open("./XCL/LatestLaunch.bat","w") as file:
+        file.write(f"@echo off\n.\\res\\frpc.exe -u {core.g_var.User.token} -p {tun_id}\npause\nexit")
+    os.system(f"start \"frpc-{tun_id}\" .\XCL\LatestLaunch.bat")
