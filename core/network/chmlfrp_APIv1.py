b4a68a4c4f14cc5110468b21b2bc118f76a64b14
boringstudent <boring_student@qq.com>
2025-01-22 16:19:01 +0800

    完善了隧道删除

--- core/network/chmlfrp_APIv1.py
+++ core/network/chmlfrp_APIv1.py
@@ -1,7 +1,8 @@
 import requests
 
 class APIv1:
-    url="https://cf-v1.uapis.cn"
+    url="https://cf-v1.uapis.cn/api"
+
     def reToken(usertoken:str)->str:
         data:dict=requests.get(APIv1.url+"/userinfo",{
             "token":usertoken
@@ -10,3 +11,16 @@ class APIv1:
             return data["newToken"]
         else:
             return None
+
+    def tunnel_deletion(usertoken:str,userid:int,nodeid:int):
+        url_v1 = f"{APIv1.url}/deletetl.php"
+        params = {
+            "token": usertoken,
+            "userid": userid,
+            "nodeid": nodeid,
+        }
+        response_v1 = requests.get(url_v1, params=params)
+        if response_v1.status_code == 200:
+            return True
+        else:
+            return False
